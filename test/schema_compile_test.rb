8b5fae0ef2b39bcd7ce1b5dc0626dd3da69398cc
Nikita Shkoda <lstpsche@gmail.com>
2026-05-13 19:37:10 +0300

    test(geo): add unit tests for geo DSL, geo distance, and syncable timing

--- test/schema_compile_test.rb
+++ test/schema_compile_test.rb
@@ -12,6 +12,14 @@ class SchemaCompileTest < Minitest::Test
     attribute :created_at, :time
   end
 
+  class GeoPlace < SearchEngine::Base
+    collection 'schema_geo_places'
+    identify_by :id
+    attribute :name, :string
+    attribute :location, :geopoint
+    attribute :locations, [:geopoint]
+  end
+
   def test_compile_builds_typesense_schema
     schema = SearchEngine::Schema.compile(Product)
 
@@ -25,5 +33,21 @@ class SchemaCompileTest < Minitest::Test
 
     assert schema.frozen?
     assert schema[:fields].frozen?
+  end
+
+  def test_geopoint_attribute_compiles_to_geopoint_type
+    schema = SearchEngine::Schema.compile(GeoPlace)
+    fields = schema[:fields]
+
+    geo_field = fields.find { |f| f[:name] == 'location' }
+    assert_equal 'geopoint', geo_field[:type]
+  end
+
+  def test_geopoint_array_attribute_compiles_to_geopoint_array_type
+    schema = SearchEngine::Schema.compile(GeoPlace)
+    fields = schema[:fields]
+
+    geo_array_field = fields.find { |f| f[:name] == 'locations' }
+    assert_equal 'geopoint[]', geo_array_field[:type]
   end
 end
