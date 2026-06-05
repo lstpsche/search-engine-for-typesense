d559a3e1ce1322c98015fd684f7193400182724b
Nikita Shkoda <lstpsche@gmail.com>
2026-06-05 14:37:42 +0300

    fix: load search engine dependencies explicitly

--- lib/search_engine/dsl/parser.rb
+++ lib/search_engine/dsl/parser.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'date'
+
 module SearchEngine
   module DSL
     # Safe parser that converts Relation#where inputs into validated AST nodes.
--- lib/search_engine/engine.rb
+++ lib/search_engine/engine.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'rails'
+
 module SearchEngine
   # Rails engine for the SearchEngine gem.
   # Configures autoloading and eager-loading paths.
